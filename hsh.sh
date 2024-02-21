d2c6acb5e10fc62fa5e8c2ebd77036c5dc579382
s0foo <s0foo@laposte.net>
2024-02-21 08:20:52 +0100

    Create hsh.sh

---
+++ hsh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Git configuration
+cp config/.gitconfig $HOME/.
+git config --global user.name "s0foo"
+git config --global user.email "s0foo@domain.com"
+
+# Welcome message
+echo "Welcome home. Take the time for a vervain."
