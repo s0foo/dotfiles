bb30da535536e8ddd4125834f3e25acb4ffe1f3d
s0foo <mtth.grd@laposte.net>
2025-04-18 15:12:45 -0400

    Init set_env script

---
+++ set_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+# git
+cp git/gitconfig $HOME/.gitconfig
