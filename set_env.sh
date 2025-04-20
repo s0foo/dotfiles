640a41cdc24b5585a8ba1fa01164e3dcf83a157d
s0foo <mtth.grd@laposte.net>
2025-04-20 14:40:09 -0400

    Include nvim configuration

--- set_env.sh
+++ set_env.sh
@@ -4,6 +4,7 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+cp -r nvim $HOME/.config/.
 
 # git
 cp git/gitconfig $HOME/.gitconfig
