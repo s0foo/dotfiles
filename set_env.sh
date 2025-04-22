af7f4f1fdce4aa65ee0b09df36e3b70a6041f707
Matthieu <176236412+s0foo@users.noreply.github.com>
2025-04-22 11:39:38 +0200

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# packages
+sudo apt install -y curl git
+
 # neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 cp -r nvim $HOME/.config/.
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # git
 cp git/gitconfig $HOME/.gitconfig
