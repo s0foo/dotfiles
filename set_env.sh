50babfbbd51b1a8f8651d87feeca367ed2cf096c
s0foo <mtth.grd@laposte.net>
2025-04-22 15:11:15 -0400

    Neovim from source

--- set_env.sh
+++ set_env.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# packages
-sudo apt install -y curl git
-
-# neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-cp -r nvim $HOME/.config/.
-
-# rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# requirements
+sudo apt install -y \
+    build-essential \
+    cmake \
+    curl \
+    gettext \
+    git \
+    ninja-build
 
 # git
 cp git/gitconfig $HOME/.gitconfig
+
+# neovim
+cp -r nvim $HOME/.config/.
+git clone -b stable --depth 1 https://github.com/neovim/neovim $HOME/neovim
+cd $HOME/neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
