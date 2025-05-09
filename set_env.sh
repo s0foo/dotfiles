0fd596330caf018982cecb0a8187d57fd0ae3b50
s0foo <mtth.grd@laposte.net>
2025-05-09 16:52:41 +0200

    add options

--- set_env.sh
+++ set_env.sh
@@ -11,14 +11,34 @@ sudo apt install -y \
     git \
     ninja-build
 
-# git
-cp $CURRENT_PATH/git/gitconfig $HOME/.gitconfig
+opt_git() {
+    cp $CURRENT_PATH/git/gitconfig $HOME/.gitconfig
+}
 
-# neovim
-cp -r $CURRENT_PATH/nvim $HOME/.config/.
-git clone -b stable --depth 1 https://github.com/neovim/neovim $HOME/neovim
-cd $HOME/neovim
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install
-cd $CURRENT_PATH
-rm -rf $HOME/neovim
+opt_nvim() {
+    cp -r $CURRENT_PATH/nvim $HOME/.config/.
+    git clone -b stable --depth 1 https://github.com/neovim/neovim $HOME/neovim
+    cd $HOME/neovim
+    make CMAKE_BUILD_TYPE=RelWithDebInfo
+    sudo make install
+    cd $CURRENT_PATH
+    rm -rf $HOME/neovim
+}
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --git)
+            opt_git
+            shift
+            ;;
+        --nvim)
+            opt_nvim
+            shift
+            ;;
+        *)
+            echo "Unknown option or argument: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
